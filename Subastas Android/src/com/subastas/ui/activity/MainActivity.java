da8f4d007c86059759b0040f7e7d112e86cff767
Jose <joshk44@icloud.com>
2014-05-28 04:55:55 -0300

    Add internal classes in order to manage tab titles.

--- Subastas Android/src/com/subastas/ui/activity/MainActivity.java
+++ Subastas Android/src/com/subastas/ui/activity/MainActivity.java
@@ -1,16 +1,17 @@
 package com.subastas.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
 import com.subastas.R;
 import com.subastas.ui.adapter.MyFragmentPagerAdapter;
+import com.subastas.ui.component.SlidingTabLayout;
+import com.subastas.ui.component.SlidingTabLayout.TabColorizer;
 import com.subastas.ui.fragment.ActualProducts;
 import com.subastas.ui.fragment.FinishedProducts;
 import com.subastas.ui.fragment.IncomingProducts;
-import com.viewpagerindicator.TabPageIndicator;
-import com.viewpagerindicator.UnderlinePageIndicator;
 
 public class MainActivity extends FragmentActivity {
 
@@ -21,14 +22,35 @@ public class MainActivity extends FragmentActivity {
 		MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(
 				this.getSupportFragmentManager(), true);
 		try {
-			adapter.addFragment(new FinishedProducts(), "Proximos");
-			adapter.addFragment(new ActualProducts(), "Actual");
-			adapter.addFragment(new IncomingProducts(), "Finalizado");
+			adapter.addFragment(new FinishedProducts(), "Proximas Subastas");
+			adapter.addFragment(new ActualProducts(), "Subastas Activas");
+			adapter.addFragment(new IncomingProducts(), "Subastas Finalizadas");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		pager.setAdapter(adapter);
-		TabPageIndicator  titleIndicator = (TabPageIndicator) findViewById(R.id.indicator);
+		SlidingTabLayout  titleIndicator = (SlidingTabLayout) findViewById(R.id.indicator);
+		titleIndicator.setCustomTabColorizer(new TabColorizer() {
+			
+			@Override
+			public int getIndicatorColor(int position) {
+				switch (position){
+				case 0:
+					return Color.RED;
+				case 1:
+					return Color.BLUE;
+				case 2:
+					return Color.GREEN;
+				}
+				return 0;
+			}
+			
+			@Override
+			public int getDividerColor(int position) {
+				
+				return Color.GRAY;
+			}
+		});
 		titleIndicator.setViewPager(pager);
 	}
 }
